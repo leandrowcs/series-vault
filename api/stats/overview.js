06a17df5872a58b49010422196033a9d56153965
Leandro Wanderley <leandrowcs@hotmail.com>
2026-08-11 13:18:42 -0400

    refactor: remove deprecated API endpoints and consolidate TMDB functionality into a single handler

--- api/stats/overview.js
+++
@@ -1,8 +0,0 @@
-const { sendJson } = require('../_tmdb')
-
-module.exports = function handler(_req, res) {
-  sendJson(res, 200, {
-    total_watched_episodes: 0,
-    total_runtime_minutes: 0,
-  })
-}
--- api/stats/years.js
+++
@@ -1,5 +0,0 @@
-const { sendJson } = require('../_tmdb')
-
-module.exports = function handler(_req, res) {
-  sendJson(res, 200, [])
-}
